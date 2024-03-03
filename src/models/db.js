e1d0d090bfb914c9a461516e80a28b8d8728f047
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2024-03-04 00:54:29 +0800

    Fix mismatch of variable names in env and db.js

--- src/models/db.js
+++ src/models/db.js
@@ -2,5 +2,5 @@ import "dotenv/config"
 import mongoose from "mongoose"
 
 export const connect = () => {
-    mongoose.connect(process.env.MONGODB_URI)
+    mongoose.connect(process.env.MONGO_URI)
 }
