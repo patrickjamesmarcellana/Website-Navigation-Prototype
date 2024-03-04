38dae2c60f301db4f78804761d573ffa993c0452
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2024-03-05 00:20:23 +0800

    complete populating nesting 3 of 'managing your account' main menu

--- src/models/populate.js
+++ src/models/populate.js
@@ -43,8 +43,8 @@ async function main() {
     
         // populate nesting 2 
 
-        // using facebook
         try {
+            // using facebook
             var yourProfile2 = await Menu.create({
                 name: "Your Profile",
                 parentMenu: usingFacebook1,
@@ -163,6 +163,230 @@ async function main() {
             console.error(err)
         }
         
+        // populate nesting 3
+
+        // using facebook
+        try {
+            // your profile
+            var addAndEditYourProfile3 = await Menu.create({
+                name: "Add and Edit Your Profile Info",
+                parentMenu: yourProfile2,
+                nestLevel: 3
+            })
+            await addAndEditYourProfile3.populate("parentMenu")
+
+            var yourProfilePicture3 = await Menu.create({
+                name: "Your Profile Picture and Cover Photo",
+                parentMenu: yourProfile2,
+                nestLevel: 3
+            })
+            await yourProfilePicture3.populate("parentMenu")
+
+            var shareAndManagePosts3 = await Menu.create({
+                name: "Share and Manage Posts on Your Profile",
+                parentMenu: yourProfile2,
+                nestLevel: 3
+            })
+            await shareAndManagePosts3.populate("parentMenu")
+
+            // messaging 
+            var sendMessages3 = await Menu.create({
+                name: "Send Messages",
+                parentMenu: messaging2,
+                nestLevel: 3
+            })
+            await sendMessages3.populate("parentMenu")
+
+            var viewAndManageMessages3 = await Menu.create({
+                name: "View and Manage Messages",
+                parentMenu: messaging2,
+                nestLevel: 3
+            })
+            await viewAndManageMessages3.populate("parentMenu")
+
+            var reportAMessage3 = await Menu.create({
+                name: "Report a Message",
+                parentMenu: messaging2,
+                nestLevel: 3
+            })
+            await reportAMessage3.populate("parentMenu")
+
+            var videoCalling3 = await Menu.create({
+                name: "Video Calling",
+                parentMenu: messaging2,
+                nestLevel: 3
+            })
+            await videoCalling3.populate("parentMenu")
+
+            // pages
+            var createAndManageAPage3 = await Menu.create({
+                name: "Create and Manage a Page",
+                parentMenu: pages2,
+                nestLevel: 3
+            })
+            await createAndManageAPage3.populate("parentMenu")
+
+            var managePageSettings3 = await Menu.create({
+                name: "Manage Page Settings",
+                parentMenu: pages2,
+                nestLevel: 3
+            })
+            await managePageSettings3.populate("parentMenu")
+
+            var customizeAPage3 = await Menu.create({
+                name: "Customize a Page",
+                parentMenu: pages2,
+                nestLevel: 3
+            })
+            await customizeAPage3.populate("parentMenu")
+
+            var publishing3 = await Menu.create({
+                name: "Publishing",
+                parentMenu: pages2,
+                nestLevel: 3
+            })
+            await publishing3.populate("parentMenu")
+
+            // groups
+            var postParticipate3 = await Menu.create({
+                name: "Post, Participate, and Privacy",
+                parentMenu: groups2,
+                nestLevel: 3
+            })
+            await postParticipate3.populate("parentMenu")
+
+            var createEngage3 = await Menu.create({
+                name: "Create, Engage, and Manage Settings",
+                parentMenu: groups2,
+                nestLevel: 3
+            })
+            await createEngage3.populate("parentMenu")
+
+            var managePeopleAndContent3 = await Menu.create({
+                name: "Manage People and Content",
+                parentMenu: groups2,
+                nestLevel: 3
+            })
+            await managePeopleAndContent3.populate("parentMenu")
+
+            var communityChats3 = await Menu.create({
+                name: "Community Chats",
+                parentMenu: groups2,
+                nestLevel: 3
+            })
+            await communityChats3.populate("parentMenu")
+        } catch (err) {
+            console.error(err)
+        }
+
+        // managing your account
+
+        try {
+            // login and password
+            var logIntoYourAccount3 = await Menu.create({
+                name: "Log Into Your Account",
+                parentMenu: loginAndPassword2,
+                nestLevel: 3
+            })
+            await logIntoYourAccount3.populate("parentMenu")
+
+            var changeYourPassword3 = await Menu.create({
+                name: "Change Your Password",
+                parentMenu: loginAndPassword2,
+                nestLevel: 3
+            })
+            await changeYourPassword3.populate("parentMenu")
+
+            var fixALoginProblem3 = await Menu.create({
+                name: "Fix a Login Problem",
+                parentMenu: loginAndPassword2,
+                nestLevel: 3
+            })
+            await fixALoginProblem3.populate("parentMenu")
+
+            var uploadingYourID3 = await Menu.create({
+                name: "Uploading Your ID",
+                parentMenu: loginAndPassword2,
+                nestLevel: 3
+            })
+            await uploadingYourID3.populate("parentMenu")
+
+            // account settings
+            var adjustYourAccountSettings3 = await Menu.create({
+                name: "Adjust Your Account Settings",
+                parentMenu: accountSettings2,
+                nestLevel: 3
+            })
+            await adjustYourAccountSettings3.populate("parentMenu")
+
+            var connectToFacebookWithout3 = await Menu.create({
+                name: "Connect to Facebook Without Data Charges",
+                parentMenu: accountSettings2,
+                nestLevel: 3
+            })
+            await connectToFacebookWithout3.populate("parentMenu")
+
+            var yourUsername3 = await Menu.create({
+                name: "Your Username",
+                parentMenu: accountSettings2,
+                nestLevel: 3
+            })
+            await yourUsername3.populate("parentMenu")
+
+            var legacyContacts3 = await Menu.create({
+                name: "Legacy Contacts",
+                parentMenu: accountSettings2,
+                nestLevel: 3
+            })
+            await legacyContacts3.populate("parentMenu")
+
+            // notifications
+            var pushEmailAndText3 = await Menu.create({
+                name: "Push, Email, and Text Notifications",
+                parentMenu: notifications2,
+                nestLevel: 3
+            })
+            await pushEmailAndText3.populate("parentMenu")
+
+            var chooseWhatYoureNotified3 = await Menu.create({
+                name: "Choose What You're Notified About",
+                parentMenu: notifications2,
+                nestLevel: 3
+            })
+            await chooseWhatYoureNotified3.populate("parentMenu")
+
+            var fixAProblem3 = await Menu.create({
+                name: "Fix a Problem",
+                parentMenu: notifications2,
+                nestLevel: 3
+            })
+            await fixAProblem3.populate("parentMenu")
+
+            // ad preferences
+            var howAdsWork3 = await Menu.create({
+                name: "How Ads Work on Facebook",
+                parentMenu: adPreferences2,
+                nestLevel: 3
+            })
+            await howAdsWork3.populate("parentMenu")
+
+            var controlTheAds3 = await Menu.create({
+                name: "Control the Ads You See",
+                parentMenu: adPreferences2,
+                nestLevel: 3
+            })
+            await controlTheAds3.populate("parentMenu")
+
+            var yourInfoAndFacebook3 = await Menu.create({
+                name: "Your Info and Facebook Ads",
+                parentMenu: adPreferences2,
+                nestLevel: 3
+            })
+            await yourInfoAndFacebook3.populate("parentMenu")
+
+        } catch (err) {
+            console.error(err) 
+        }
     
         console.log("facebook-help-center db repopulated successfully")
         
