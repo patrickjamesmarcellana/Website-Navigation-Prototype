8ed903b813f75b04b8aff13cd3420baa4c7820d7
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2024-03-05 22:19:41 +0800

    Delete old click listeners in helpcenter.js

--- public/js/helpcenter.js
+++ public/js/helpcenter.js
@@ -1,20 +1,3 @@
-// $("#UsingFacebook").click((e) => {
-//     console.log("Success")
-//     $("#yourProfile").removeClass("hidden")
-// })
-
-// $("#yourProfile").click((e) => {
-//     $("#addAndEdit").removeClass("hidden")
-// })
-
-// $("#addAndEdit").click((e) => {
-//     $("#targetPage").removeClass("hidden")
-// })
-
-// $("#doneBtn").click((e) => {
-//     window.location.replace("done.html")
-// })
-
 $(".menu").click(async (e) => {
     // e.stopPropagtation()
     const selectedMenu = e.target;
