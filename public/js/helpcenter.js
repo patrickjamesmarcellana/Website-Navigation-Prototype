b7ed63219573ac0926956e2714424f3a67ba43c7
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2024-03-06 03:59:05 +0800

    Implement path counting

--- public/js/helpcenter.js
+++ public/js/helpcenter.js
@@ -1,6 +1,32 @@
+var previouslySelected;
+var paths = 0;
+
 $(".menu").click(async (e) => {
-    // e.stopPropagtation()
+    // selected menu
     const selectedMenu = e.target;
+    const selectedMenuId = selectedMenu.getAttribute("menu-id");
+    let response = await fetch("/getMenu/" + selectedMenuId)
+    if(response.status == 400) {
+        console.log("Error in fetching selected menu data from database.")
+        return
+    }
+    const selectedMenuData = await response.json()
+
+    // previously selected menu
+    let previouslySelectedId, previouslySelectedData;
+    if(previouslySelected) {
+        previouslySelectedId = previouslySelected.getAttribute("menu-id")
+        response = await fetch("/getMenu/" + previouslySelectedId)
+        if(response.status == 400) {
+            console.log("Error in fetching previously selected menu data from database.")
+            return
+        }
+        previouslySelectedData = await response.json()
+    }
+
+    console.log("Selected ID: " + selectedMenuId)
+    console.log("Selected Parent: " + selectedMenuData.parentMenu)
+    console.log("Previous Selected ID: " + previouslySelectedId)
 
     // if selected menu is already expanded, hide its submenus (not grandchildren)
     if (selectedMenu.classList.contains("expanded")) {
@@ -17,13 +43,27 @@ $(".menu").click(async (e) => {
             subMenu.classList.remove("hidden");
         }
         selectedMenu.classList.add("expanded");
+
+        // update paths (backtracking) count
+        if (previouslySelected && // defensive, displaying hidden submenus should not happen on initial state (i.e., no menu clicked yet at all)
+            !selectedMenuData.isLeaf &&
+            previouslySelectedId !== selectedMenuId && // opening, closing, opening the same menu consecutively should not increase path count
+            selectedMenuData.parentMenu !== previouslySelected.getAttribute("menu-id") // clicked menu's parent is the previously clicked menu means the same path
+        ) {
+            if(selectedMenuData.nestLevel === previouslySelectedData.nestLevel &&
+              selectedMenuData.parentMenu === previouslySelectedData.parentMenu ||
+              selectedMenuData.parentMenu !== previouslySelectedData.parentMenu) {
+                paths += 1
+            }
+        }
+
+        console.log("Path count: " + paths)
+        previouslySelected = selectedMenu;
         return;
     }
 
     // first time accessing the menu and its submenus; add them as menu's children
-    const selectedMenuId = selectedMenu.getAttribute("menu-id");
-    console.log(selectedMenuId);
-    const response = await fetch("/getMenu/" + selectedMenuId);
+    response = await fetch("/getSubMenus/" + selectedMenuId);
     console.log(response.status);
 
     if (response.status === 200) {
@@ -39,5 +79,23 @@ $(".menu").click(async (e) => {
 
         // add expanded class to prevent adding the submenus again
         selectedMenu.classList.add("expanded");
+
+        // update paths (backtracking) count
+        if (previouslySelected && // defensive, displaying hidden submenus should not happen on initial state (i.e., no menu clicked yet at all)
+            !selectedMenuData.isLeaf && // clicking on leaves does not continue or open a path
+            previouslySelectedId !== selectedMenuId && // opening, closing, opening the same menu consecutively should not increase path count
+            selectedMenuData.parentMenu !== previouslySelected.getAttribute("menu-id") // clicked menu's parent is the previously clicked menu means the same path
+        ) {
+            console.log("PASSED HERE")
+            if(selectedMenuData.nestLevel === previouslySelectedData.nestLevel &&
+              selectedMenuData.parentMenu === previouslySelectedData.parentMenu ||
+              selectedMenuData.parentMenu !== previouslySelectedData.parentMenu) {
+                paths += 1
+            }
+        }
+
+        console.log("Path count: " + paths)
+        previouslySelected = selectedMenu;
+        return;
     }
 });
--- src/routes/menu.js
+++ src/routes/menu.js
@@ -2,6 +2,7 @@ import express from "express"
 const menuRouter = express.Router()
 import { connect, disconnect } from "../models/db.js"
 import Menu from "../models/menu.js"
+import menu from "../models/menu.js"
 
 const menusToJson = async (documents) => {
     const json = []
@@ -19,17 +20,44 @@ const menuToJson = async (document) => {
         menuId: document._id,
         name: document.name,
         divId: document.name.replace(/ +/g, ""),
-        leftPadding: (document.nestLevel - 1) * 15
+        leftPadding: (document.nestLevel - 1) * 15,
+
+        parentMenu: document.parentMenu,
+        nestLevel: document.nestLevel,
+        isLeaf: document.isLeaf
     }
 }
 
-menuRouter.get("/getMenu/:menuId", async (req, res) => {
-    console.log("Request for menu by id", req.params.menuId)
+menuRouter.get("/getSubMenus/:menuId", async (req, res) => {
+    console.log("Request for submenus by parent menu id", req.params.menuId)
     try {
         await connect()
         const menuToExpand = await Menu.find({_id: req.params.menuId})
         const subMenus = await Menu.find({parentMenu: menuToExpand})
         var json = await  menusToJson(subMenus)
+        console.log(json)
+    } catch (err) {
+        console.error(err) 
+    }
+
+    try {
+        await disconnect()
+    } catch (err) {
+        console.error(err)
+    }
+
+    if(json != null)
+        res.send(json)
+    else
+        res.sendStatus(400)
+})
+
+menuRouter.get("/getMenu/:menuId", async (req, res) => {
+    console.log("Request for menu by menu id", req.params.menuId)
+    try {
+        await connect()
+        const menu = await Menu.findById(req.params.menuId)
+        var json = await  menuToJson(menu)
         console.log(json)
     } catch (err) {
         console.error(err) 
