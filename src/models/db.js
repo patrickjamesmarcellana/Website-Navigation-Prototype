60a5aa23ff2042666a692d3c8925b3b317e1761e
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2024-03-28 17:11:00 +0800

    Change variable name

--- src/models/db.js
+++ src/models/db.js
@@ -2,7 +2,7 @@ import "dotenv/config"
 import mongoose from "mongoose"
 
 export const connect = async () => {
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(process.env.MONGODB_URI)
     console.log("MongoDB Connected.")
 }
 
